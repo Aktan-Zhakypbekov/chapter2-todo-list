e73128c726754c0820e59690e2ee1d52a98e4fef
Aktan Zhakypbekov <aktanzhakypbekov@gmail.com>
2021-10-07 01:05:17 +0600

    ...

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ let projectsArray = [];
 //display option interfaces
 
 let optionPressed = false;
-let defaultOptionsList = document.querySelectorAll(".option");
-defaultOptionsList.forEach((option) => {
-  option.addEventListener("click", (e) => {
+let defaultProjectsList = document.querySelectorAll(".project");
+defaultProjectsList.forEach((project) => {
+  project.addEventListener("click", (e) => {
     if (optionPressed) {
-      document.querySelector(".option-interface").remove();
+      document.querySelector(".project-interface").remove();
       optionPressed = false;
     }
-    displayInterface(option.id);
+    displayProjectInterface(project.id);
   });
 });
 
-function displayInterface(optionId) {
-  let optionInterface = document.createElement("div");
-  optionInterface.className = "option-interface";
-  optionInterface.style.cssText =
+function displayProjectInterface(projectId) {
+  let projectInterface = document.createElement("div");
+  projectInterface.className = "project-interface";
+  projectInterface.style.cssText =
     "background-color: lightgreen; height: 100%; width: 100%;";
 
-  let interfaceHeader = document.createElement("div");
-  interfaceHeader.className = "interface-header";
-  interfaceHeader.textContent = optionId;
-  interfaceHeader.style.cssText = "height: 30px; background-color: orange;";
-  optionInterface.appendChild(interfaceHeader);
+  let projectInterfaceHeader = document.createElement("div");
+  projectInterfaceHeader.className = "project-interface-header";
+  projectInterfaceHeader.textContent = projectId;
+  projectInterfaceHeader.style.cssText =
+    "height: 30px; background-color: orange;";
+  projectInterface.appendChild(projectInterfaceHeader);
 
-  document.querySelector("#display").appendChild(optionInterface);
+  document.querySelector("#display").appendChild(projectInterface);
   optionPressed = true;
 }
 
@@ -55,9 +56,22 @@ class CreateProjects {
 let submitProjectButton = document.querySelector("#submit-project-button");
 submitProjectButton.addEventListener("click", (e) => {
   e.preventDefault();
+  addToProjectsArray();
+});
+
+function addToProjectsArray() {
   let projectTitle = document.querySelector("#project-title").value;
   let project = new CreateProjects(projectTitle);
   projectsArray.push(project);
   addProjectFormCont.style.cssText = "display: none;";
-});
-console.log(projectsArray);
+  addToUserProjectsMenu(projectTitle);
+}
+function addToUserProjectsMenu(projectTitle) {
+  let projectItem = document.createElement("button");
+  projectItem.className = "project-item";
+  projectItem.style.cssText =
+    "width: 50px; height: 20px; background-color: red;";
+  projectItem.textContent = projectTitle;
+  let userProjectsMenu = document.querySelector(".menu__user-projects");
+  userProjectsMenu.appendChild(projectItem);
+}
