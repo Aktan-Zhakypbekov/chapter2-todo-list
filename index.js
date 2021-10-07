1b7b448e561863992f63991c8d7df58fe0f85dc2
Aktan Zhakypbekov <aktanzhakypbekov@gmail.com>
2021-10-08 00:27:16 +0600

    In progress

--- index.js
+++ index.js
@@ -1,77 +1,132 @@
-let projectsArray = [];
+displayMenuProjects();
 
-//display option interfaces
+let projectsArray = JSON.parse(localStorage.getItem("projects"));
 
-let optionPressed = false;
-let defaultProjectsList = document.querySelectorAll(".project");
-defaultProjectsList.forEach((project) => {
-  project.addEventListener("click", (e) => {
-    if (optionPressed) {
-      document.querySelector(".project-interface").remove();
-      optionPressed = false;
-    }
-    displayProjectInterface(project.id);
-  });
+let addProjectFormContainer = document.querySelector(".add-project-form-cont");
+let menuAddProjectButton = document.querySelector(".menu__add-project-button");
+menuAddProjectButton.addEventListener("click", (e) => {
+  addProjectFormContainer.style.cssText = "display: flex;";
 });
 
-function displayProjectInterface(projectId) {
+let addProjectFormExitButton = document.querySelector(
+  ".add-project-form-cont__exit-cont__exit-button"
+);
+addProjectFormExitButton.addEventListener("click", (e) => {
+  addProjectFormContainer.style.cssText = "display: none;";
+});
+
+let addProjectFormSubmitButton = document.querySelector(
+  ".add-project-form-cont__form__submit-button"
+);
+addProjectFormSubmitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  addToProjectsArray();
+  displayMenuProjects();
+});
+
+class CreateProject {
+  constructor(title) {
+    this.title = title;
+    this.todoList = [];
+  }
+}
+
+function addToProjectsArray() {
+  let projectTitle = document.querySelector(
+    ".add-project-form-cont__form__project-title"
+  ).value;
+  let project = new CreateProject(projectTitle);
+  projectsArray.push(project);
+  localStorage.setItem("projects", JSON.stringify(projectsArray));
+}
+
+let optionPressed = false;
+
+function displayMenuProjects() {
+  let menuProjects = document.querySelectorAll(".project");
+  menuProjects.forEach((item) => {
+    item.remove();
+  });
+
+  let projectsList = JSON.parse(localStorage.getItem("projects"));
+  projectsList.forEach((item) => {
+    let project = document.createElement("button");
+    project.className = "project";
+    project.style.cssText = "background-color: red;";
+    project.textContent = item.title;
+
+    project.addEventListener("click", (e) => {
+      if (optionPressed) {
+        document.querySelector(".project-interface").remove();
+        optionPressed = false;
+      }
+      displayProjectInterface(item.title);
+      optionPressed = true;
+    });
+
+    document.querySelector(".menu").appendChild(project);
+  });
+}
+
+function displayProjectInterface(title) {
   let projectInterface = document.createElement("div");
   projectInterface.className = "project-interface";
   projectInterface.style.cssText =
-    "background-color: lightgreen; height: 100%; width: 100%;";
-
+    "width: 100%; height: 100%; background-color: blue; display: flex; flex-direction: column; align-items: center;";
   let projectInterfaceHeader = document.createElement("div");
+  projectInterfaceHeader.textContent = title;
   projectInterfaceHeader.className = "project-interface-header";
-  projectInterfaceHeader.textContent = projectId;
-  projectInterfaceHeader.style.cssText =
-    "height: 30px; background-color: orange;";
-  projectInterface.appendChild(projectInterfaceHeader);
+  projectInterfaceHeader.style.cssText = "background-color: green;";
 
-  document.querySelector("#display").appendChild(projectInterface);
-  optionPressed = true;
+  let projectInterfaceAddToDoButton = document.createElement("button");
+  projectInterfaceAddToDoButton.className =
+    "project-interface-add-to-do-button";
+  projectInterfaceAddToDoButton.textContent = "Add to-do";
+  projectInterfaceAddToDoButton.style.cssText = "violet";
+  projectInterfaceAddToDoButton.addEventListener("click", (e) => {
+    addToDoFormContainer.style.cssText = "display: flex;";
+  });
+
+  projectInterface.appendChild(projectInterfaceHeader);
+  projectInterface.appendChild(projectInterfaceAddToDoButton);
+  let display = document.querySelector(".display");
+  display.appendChild(projectInterface);
 }
 
-//add new projects
-
-let addProjectFormCont = document.querySelector("#add-project-form-cont");
-
-let addProjectButton = document.querySelector("#add-project-button");
-addProjectButton.addEventListener("click", (e) => {
-  addProjectFormCont.style.cssText = "display: flex";
+let addToDoFormContainer = document.querySelector(".add-to-do-form-cont");
+let addToDoFormExitButton = document.querySelector(
+  ".add-to-do-form-cont__exit-cont__exit-button"
+);
+addToDoFormExitButton.addEventListener("click", (e) => {
+  addToDoFormContainer.style.cssText = "display: none;";
 });
 
-let formExitButton = document.querySelector("#form-exit-button");
-formExitButton.addEventListener("click", (e) => {
-  addProjectFormCont.style.cssText = "display: none;";
+let addToDoFormSubmitButton = document.querySelector(
+  ".add-to-do-form-cont__form__submit-button"
+);
+addToDoFormSubmitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  addToDoToProjectsArrayMenuProjects();
 });
 
-// projects array
-
-class CreateProjects {
+class CreateToDo {
   constructor(title) {
     this.title = title;
   }
 }
 
-let submitProjectButton = document.querySelector("#submit-project-button");
-submitProjectButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  addToProjectsArray();
-});
-
-function addToProjectsArray() {
-  let projectTitle = document.querySelector("#project-title").value;
-  let project = new CreateProjects(projectTitle);
-  projectsArray.push(project);
-  addProjectFormCont.style.cssText = "display: none;";
-  addToUserProjectsMenu(projectTitle);
-}
-function addToUserProjectsMenu(projectTitle) {
-  let projectItem = document.createElement("button");
-  projectItem.className = "project-item";
-  projectItem.style.cssText =
-    "width: 50px; height: 20px; background-color: red;";
-  projectItem.textContent = projectTitle;
-  let userProjectsMenu = document.querySelector(".menu__user-projects");
-  userProjectsMenu.appendChild(projectItem);
+function addToDoToProjectsArrayMenuProjects() {
+  let toDoTitle = document.querySelector(
+    ".add-to-do-form-cont__form__to-do-title"
+  ).value;
+  let toDo = new CreateToDo(toDoTitle);
+  let projectInterfaceHeader = document.querySelector(
+    ".project-interface-header"
+  );
+  projectsArray[
+    projectsArray.findIndex(
+      (x) => x.title == projectInterfaceHeader.textContent
+    )
+  ].todoList.push(toDo);
+  localStorage.setItem("projects", JSON.stringify(projectsArray));
 }
