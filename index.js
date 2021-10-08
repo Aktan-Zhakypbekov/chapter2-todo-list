f55aa35610f62744e411a197ad3dd34898814499
Aktan Zhakypbekov <aktanzhakypbekov@gmail.com>
2021-10-09 03:33:01 +0600

    ...

--- index.js
+++ index.js
@@ -50,20 +50,38 @@ function displayMenuProjects() {
 
   let projectsList = JSON.parse(localStorage.getItem("projects"));
   projectsList.forEach((item) => {
-    let project = document.createElement("button");
+    let project = document.createElement("div");
     project.className = "project";
-    project.style.cssText = "background-color: red;";
-    project.textContent = item.title;
+    project.style.cssText =
+      "display: flex; color: rgb(47, 0, 255); background-color: black; width: 220px; height: 35px; margin: 0.5rem; border: 1px solid rgb(47, 0, 255);";
+    let projectEnter = document.createElement("button");
+    projectEnter.className = "project-enter-button";
+    projectEnter.style.cssText =
+      "width: 100px; background-color: black; color: rgb(47, 0, 255);";
+    projectEnter.textContent = item.title;
+    let projectEdit = document.createElement("button");
+    projectEdit.className = "project-edit-button";
+    projectEdit.style.cssText =
+      "flex-grow: 1; background-color: black; color: rgb(47, 0, 255);";
+    projectEdit.textContent = "Edit";
+    let projectDelete = document.createElement("button");
+    projectDelete.className = "project-edit-button";
+    projectDelete.style.cssText =
+      "flex-grow: 1; background-color: black; color: rgb(47, 0, 255);";
+    projectDelete.textContent = "Delete";
 
-    project.addEventListener("click", (e) => {
+    projectEnter.addEventListener("click", (e) => {
       if (optionPressed) {
         document.querySelector(".project-interface").remove();
         optionPressed = false;
       }
       displayProjectInterface(item.title);
+      displayToDoInProject(item);
       optionPressed = true;
     });
-
+    project.appendChild(projectEnter);
+    project.appendChild(projectEdit);
+    project.appendChild(projectDelete);
     document.querySelector(".menu").appendChild(project);
   });
 }
@@ -72,23 +90,39 @@ function displayProjectInterface(title) {
   let projectInterface = document.createElement("div");
   projectInterface.className = "project-interface";
   projectInterface.style.cssText =
-    "width: 100%; height: 100%; background-color: blue; display: flex; flex-direction: column; align-items: center;";
+    "width: 100%; height: 100%; color:rgb(47, 0, 255); background-color: lightgreen; display: flex; flex-direction: column; align-items: center;";
+
   let projectInterfaceHeader = document.createElement("div");
-  projectInterfaceHeader.textContent = title;
   projectInterfaceHeader.className = "project-interface-header";
-  projectInterfaceHeader.style.cssText = "background-color: green;";
+  projectInterfaceHeader.style.cssText =
+    "width: 100%; height: 35px; background-color: black; color: rgb(47, 0, 255); display: flex;";
+
+  let projectInterfaceHeaderTitleCont = document.createElement("div");
+  projectInterfaceHeaderTitleCont.className =
+    "project-interface-header-title-cont;";
+  projectInterfaceHeaderTitleCont.style.cssText =
+    "background-color: black; color: rgb(47, 0, 255); width: 80%; display: flex; justify-content: center; align-items: center;";
+
+  let projectInterfaceHeaderTitle = document.createElement("div");
+  projectInterfaceHeaderTitle.className = "project-interface-header-title";
+  projectInterfaceHeaderTitle.style.cssText = "font-size: 1rem;";
+  projectInterfaceHeaderTitle.textContent = title;
+  projectInterfaceHeaderTitleCont.appendChild(projectInterfaceHeaderTitle);
 
   let projectInterfaceAddToDoButton = document.createElement("button");
   projectInterfaceAddToDoButton.className =
     "project-interface-add-to-do-button";
-  projectInterfaceAddToDoButton.textContent = "Add to-do";
-  projectInterfaceAddToDoButton.style.cssText = "violet";
+  projectInterfaceAddToDoButton.textContent = "+ Add to-do";
+  projectInterfaceAddToDoButton.style.cssText =
+    "background-color: black; color: rgb(47, 0, 255); width: 20%;";
   projectInterfaceAddToDoButton.addEventListener("click", (e) => {
     addToDoFormContainer.style.cssText = "display: flex;";
   });
 
+  projectInterfaceHeader.appendChild(projectInterfaceHeaderTitleCont);
+  projectInterfaceHeader.appendChild(projectInterfaceAddToDoButton);
+
   projectInterface.appendChild(projectInterfaceHeader);
-  projectInterface.appendChild(projectInterfaceAddToDoButton);
   let display = document.querySelector(".display");
   display.appendChild(projectInterface);
 }
@@ -107,6 +141,7 @@ let addToDoFormSubmitButton = document.querySelector(
 addToDoFormSubmitButton.addEventListener("click", (e) => {
   e.preventDefault();
   addToDoToProjectsArrayMenuProjects();
+  displayMenuProjects();
 });
 
 class CreateToDo {
@@ -129,4 +164,24 @@ function addToDoToProjectsArrayMenuProjects() {
     )
   ].todoList.push(toDo);
   localStorage.setItem("projects", JSON.stringify(projectsArray));
+  document.querySelectorAll(".to-do-item").forEach((item) => {
+    item.remove();
+  });
+  displayToDoInProject(
+    projectsArray[
+      projectsArray.findIndex(
+        (x) => x.title == projectInterfaceHeader.textContent
+      )
+    ]
+  );
+}
+
+function displayToDoInProject(item1) {
+  item1.todoList.forEach((toDo) => {
+    let toDoItem = document.createElement("button");
+    toDoItem.className = "to-do-item";
+    toDoItem.style.cssText = "background-color: black; color: rgb(47, 0, 255);";
+    toDoItem.textContent = toDo.title;
+    document.querySelector(".project-interface").appendChild(toDoItem);
+  });
 }
